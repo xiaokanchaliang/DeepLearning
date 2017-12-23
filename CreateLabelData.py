caebb89d9a42a6d4c0389651b1a4f85ec8177b23
wangbo <511975222@qq.com>
2017-12-23 23:24:48 +0800

    .

---
+++ CreateLabelData.py
@@ -0,0 +1,25 @@
+import csv
+import numpy as np
+import matplotlib.pyplot as plt
+
+labelData = np.empty((1000,3))
+
+reader = csv.reader(open('qualityDataForNormalized.csv', encoding='utf-8'))
+
+count = 0
+
+for row in reader:
+    labelData[count][0] = int(float(row[0])*10)/10
+    labelData[count][1] = int(float(row[1])*10)/10
+    labelData[count][2] = int(float(row[2])*10)/10
+    count = count + 1
+
+labelDataFile=open('labelData.csv','w',newline='')
+
+labelDataWriter=csv.writer(labelDataFile)
+
+labelDataWriter.writerows(labelData)
+
+plt.plot(np.linspace(1, 1000, 1000), labelData)
+
+plt.show()
--- CreateNormalizedData.py
+++ CreateNormalizedData.py
@@ -3,16 +3,16 @@ import numpy as np
 
 processData = np.random.random((1000,13))
 
-qualityData = np.zeros((1000,3),dtype=np.float32)
+qualityData = np.zeros((1000,3), dtype=np.float32)
 
 noiseData1 = np.random.uniform(-0.1,0.1,1000)
 noiseData2 = np.random.uniform(-0.1,0.1,1000)
 noiseData3 = np.random.uniform(-0.1,0.1,1000)
 
 for i in range(1000):
-    qualityData[i][0] = processData[i][0]*0.3 + processData[i][1]*0.1 + processData[i][2]*0.05 + processData[i][3]*0.25 + processData[i][4]*0.15 + processData[i][10]*0.15
-    qualityData[i][1] = processData[i][5]*0.3 + processData[i][6]*0.1 + processData[i][7]*0.05 + processData[i][8]*0.25 + processData[i][9]*0.15 + processData[i][11]*0.15
-    qualityData[i][2] = processData[i][3]*0.3 + processData[i][4]*0.1 + processData[i][5]*0.05 + processData[i][6]*0.25 + processData[i][7]*0.15 + processData[i][12]*0.15
+    qualityData[i][0] = processData[i][0]*0.3 + processData[i][1]*0.1 + processData[i][2]*0.05 + processData[i][3]*0.25 + processData[i][4]*0.15 + processData[i][10]*0.15 + noiseData1[i]
+    qualityData[i][1] = processData[i][5]*0.3 + processData[i][6]*0.1 + processData[i][7]*0.05 + processData[i][8]*0.25 + processData[i][9]*0.15 + processData[i][11]*0.15 + noiseData2[i]
+    qualityData[i][2] = processData[i][3]*0.3 + processData[i][4]*0.1 + processData[i][5]*0.05 + processData[i][6]*0.25 + processData[i][7]*0.15 + processData[i][12]*0.15 + noiseData3[i]
 
 processDataFile=open('processDataForNormalized.csv','w',newline='')
 qualityDataFile=open('qualityDataForNormalized.csv','w',newline='')
